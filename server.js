9f607b1472013161fb904cbb877cec82b0cc2089
usha2222 <up374120@gmail.com>
2026-06-16 20:24:13 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -55,5 +55,5 @@ app.use('/api/dashboard-stats', dashboardStatsRouter);
 app.use('/api/faculty-profile', facultyProfileRouter);
 app.use('/api/student-dashboard', studentDashboardRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
